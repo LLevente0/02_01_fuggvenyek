a9135f45116f158f11d2e0261f05a1bd02e2c5b0
LLevente0 <ladanyi.levente@diak.szbi-pg.hu>
2025-02-03 08:51:26 +0100

    Szeep

--- 03_feladat.py
+++ 03_feladat.py
@@ -5,17 +5,17 @@ hogy hány darab hárommal osztható szám van, és ezzel az értékkel tér vis
  program tartalmazza a függvény hívását is!"""
 
 
-def harommal_oszthatok(lista):
-    hanyszor_fordul_elo = 0
-    for index in range(len(lista)):
-        if lista[index] % 3 == 0:
-            hanyszor_fordul_elo += 1
-    return hanyszor_fordul_elo
-
-
-
-szamok = [3, 4, 5, 6]
-print(harommal_oszthatok(szamok))
+# def harommal_oszthatok(lista):
+#     hanyszor_fordul_elo = 0
+#     for index in range(len(lista)):
+#         if lista[index] % 3 == 0:
+#             hanyszor_fordul_elo += 1
+#     return hanyszor_fordul_elo
+#
+#
+#
+# szamok = [3, 4, 5, 6]
+# print(harommal_oszthatok(szamok))
 
 
 
@@ -24,8 +24,6 @@ Alakítsd át az előző programot úgy, hogy a felhasználó által megadott sz
 tárolja el a program egy listában, és ezt értékelje ki a függvény! 
 (Az adatbeolvasás addig tartson, míg a felhasználó negatív számot nem ad meg!)"""
 
-
-
 def harommal_oszthatok(lista):
     hanyszor_fordul_elo = 0
     for index in range(len(lista)):
@@ -34,12 +32,16 @@ def harommal_oszthatok(lista):
     return hanyszor_fordul_elo
 
 
+szamok = []
+def szam_hozzaadas():
+    szam = int(input("Adj meg számokat! "))
+    szamok.append(szam)
+    print(szamok)
 
-szam = int(input("Adj meg számokat! "))
+folytat = True
+while folytat == True:
+    szam_hozzaadas()
+    if any(n < 0 for n in szamok):
+        break
 
-
-print(harommal_oszthatok(szamok))
-
-
-
-
+print(f"\nA listában {harommal_oszthatok(szamok)} darab 3-mal osztható szám van.")
---
+++ 04_feladat.py
@@ -0,0 +1,11 @@
+"""4. Feladat
+Írj egy programot, amelyben van egy 'kerulet' nevű függvény,
+ amely az egyetlen kötelező paramétere mellett fogadhat több paramétert is!
+  Az opcionális paraméterek száma alapján döntse el milyen síkidomról van szó,
+  és számolja ki a kerületét (0 tetszőleges paraméter: négyzet,
+  1 tetszőleges paraméter: téglalap, 2 tetszőleges paraméter: háromszőg, 3 vagy több tetszőleges paraméter: sokszög)!
+
+A program tartalmazzon mindegyik síkidom típusra egy-egy függvényhívást!"""
+
+
+
